8e80773797fccda366d6ae39b5bfb13418786262
t.hayakawa <hayakawa.sou.lol@gmail.com>
2021-02-08 23:47:35 +0900

    郵便番号自動入力課

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,39 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+    <div class="location">
+      <input type="text" maxlength="7" v-model="address">
+      <button @click="transition()">住所自動入力</button>
+      <p>Address: {{ postData }}</p>
     </div>
-    <router-view/>
   </div>
 </template>
 
+<script>
+import axios from "axios";
+export default {
+  data() {
+    return {
+      address:"",
+      postData:""
+    };
+  },
+  methods: {
+      async transition() {
+            const item = await axios.get(
+            `https://apis.postcode-jp.com/api/v4/postcodes/${this.address}?&apikey=0UhyXr2fh2vCgeCvl4pYcsLx4aeZbPJwFZSYjcu`);
+
+
+            this.postData = item.data[0].allAddress;
+          }
+      }
+  };
+</script>
+
+
+
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  display: flex;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
